b7b28e339863f5bd300e9014528140efbb3bbfe8
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-07-02 13:25:19 -0300

    feat: faz conexão com o banco de dados MongoDB

---
+++ src/db/Mongo.ts
@@ -0,0 +1,13 @@
+import appConfig from "../config/app.config.js";
+import mongoose from "mongoose";
+
+export default class Mongo {
+  static async connect() {
+    try {
+      await mongoose.connect(appConfig.mongo.uri);
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+    }
+  }
+}
