e4190db26cd74df3e025b0aca63a95ccb8bb9ec6
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-07-02 13:29:53 -0300

    refactor: usa process.exit() instead of process.exitCode = 1 in Mongo.ts

--- src/db/Mongo.ts
+++ src/db/Mongo.ts
@@ -8,6 +8,7 @@ export default class Mongo {
       console.log("Connected to MongoDB");
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
+      process.exit(1);
     }
   }
 }
