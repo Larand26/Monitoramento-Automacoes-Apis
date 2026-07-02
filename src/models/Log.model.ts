36fe8457509607286e732f526c57052776058f87
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-07-02 14:19:06 -0300

    feat: cria o modelo de log para o banco de dados

---
+++ src/models/Log.model.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const logSchema = new mongoose.Schema({
+  jobName: { type: String, required: true },
+  runId: { type: String, required: true },
+  environment: {
+    type: String,
+    required: false,
+    enum: ["production", "development"],
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["success", "error", "warning", "running"],
+  },
+  startedAt: { type: Date, required: true },
+  finishedAt: { type: Date, required: false },
+  durationMs: { type: Number, required: false },
+  message: { type: String, required: false },
+  details: { type: mongoose.Schema.Types.Mixed, required: false },
+});
+
+export const LogModel = mongoose.model("Log", logSchema);
